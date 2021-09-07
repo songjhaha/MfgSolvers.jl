cbff763d7a13d10ab446039e46795fa5346f9084
songjhaha <songjh96@foxmail.com>
2021-09-07 08:12:54 +0800

    add PI2 test in 1d

--- test/runtests.jl
+++ test/runtests.jl
@@ -84,7 +84,23 @@ function TwoDimTestSimple()
     F2(m) = 0
     update_Q(Du,m) = Du / F1(m)
     problem = MFGTwoDim(xmin1,xmax1,xmin2,xmax2,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem;N=100,verbose=false,method=:PI2)
+    re = solve_mfg(problem;N=100,verbose=false)
+    return re.converge
+end
+
+function OneDimTestPI2()
+    xmin = 0.0
+    xmax = 1.0
+    T = 1
+    ε = 0.05
+    m0(x) = ((x>=0.375) && (x<=0.625)) ? 1 : 0
+    uT(x) = 100min( (x-0.3)^2, (x-0.7)^2 )
+    V(x) = 0.1
+    F1(m) = 20(1+4m)^1.5
+    F2(m) = m
+    update_Q(Du,m) = Du / F1(m)
+    problem = MFGOneDim(xmin,xmax,T,ε,m0,uT,V,F1,F2,update_Q)
+    re = solve_mfg(problem; method=:PI2, verbose=false)
     return re.converge
 end
 
@@ -100,7 +116,7 @@ function TwoDimTestPI2()
     F2(m) = 0
     update_Q(Du,m) = Du / F1(m)
     problem = MFGTwoDim(xmin1,xmax1,xmin2,xmax2,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem;N=100,verbose=false)
+    re = solve_mfg(problem; method=:PI2, N=100, verbose=false)
     return re.converge
 end
 
@@ -115,5 +131,6 @@ end
 end
 
 @testset "Alogrithm2" begin
+    @test OneDimTestPI2()
     @test TwoDimTestPI2()
 end
