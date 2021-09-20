35d7572823f0e237672ef2bdf371698d93024e1b
songjhaha <songjh96@foxmail.com>
2021-09-20 12:55:37 +0800

    rearrange

--- Examples/Case.jl
+++ Examples/Case.jl
@@ -2,10 +2,12 @@ using MfgSolvers
 using LaTeXStrings
 using Plots
 
+L_inf_dist(a,b)=maximum(abs.(a-b)) # helper function for compute L inf distance
+
 """
 ################# case 1 #####################
 """
-function OneDimTest1(method::Symbol)    
+function OneDimTest1()    
     xmin = 0.0    
     xmax = 1.0    
     T = 1    
@@ -17,11 +19,11 @@ function OneDimTest1(method::Symbol)
     F2(m) = m
     update_Q(Du,m) = Du / F1(m)
     problem = MFGOneDim(xmin,xmax,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem;method=method,node=200,N=200,verbose=true) # iter 10, Q diff 7e-9
+    re = solve_mfg(problem;method=:PI1,node=200,N=200,verbose=true)
     return re
 end
 
-function OneDimTest1_fixpoint(method::Symbol)    
+function OneDimTest1_fixpoint()    
     xmin = 0.0    
     xmax = 1.0    
     T = 1    
@@ -33,22 +35,21 @@ function OneDimTest1_fixpoint(method::Symbol)
     F2(m) = m
     update_Q(Du,m) = Du / F1(m)
     problem = MFGOneDim(xmin,xmax,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg_fixpoint(problem;method=method,node=200,N=200,verbose=true) # iter 10, Q diff 7e-9
+    re = solve_mfg_fixpoint(problem;method=:FixPoint2,node=200,N=200,verbose=true)
     return re
 end
 
-re_OneDim = OneDimTest1(:PI1)
+re_OneDim = OneDimTest1()
 
 # q* get when |q^{k+1}-q^{k}|<1e-10 with fixed point
-re_OneDim_fixpoint = OneDimTest1_fixpoint(:FixPoint2)
-# case1
-begin
-    
-    plot(re_OneDim.history.hist_q, yaxis=:log, label=L"\Vert q^{(k+1)}-q^{(k)} \Vert")
-    plot!(re_OneDim.history.hist_m, yaxis=:log, label=L"\Vert m^{(k+1)}-m^{(k)} \Vert")
-    plot!(re_OneDim.history.hist_u, yaxis=:log, label=L"\Vert u^{(k+1)}-u^{(k)} \Vert")
-    savefig("converge_case1.pdf")
+re_OneDim_fixpoint = OneDimTest1_fixpoint()
 
+# case1
+begin 
+    # plot(re_OneDim.history.hist_q, yaxis=:log, label=L"\Vert q^{(k+1)}-q^{(k)} \Vert")
+    # plot!(re_OneDim.history.hist_m, yaxis=:log, label=L"\Vert m^{(k+1)}-m^{(k)} \Vert")
+    # plot!(re_OneDim.history.hist_u, yaxis=:log, label=L"\Vert u^{(k+1)}-u^{(k)} \Vert")
+    # savefig("converge_case1.pdf")
     begin
         m_pic = plot(re_OneDim.sgrid, re_OneDim.M[:,1], label="t=0")
         for ti in [41,81,121,161,201]
@@ -67,10 +68,9 @@ begin
     plot(re_OneDim.history.residual_HJB, yaxis=:log, label="HJB")
     plot!(re_OneDim.history.residual_FP, yaxis=:log, label="FP")
     savefig("figures/case1_residual.pdf")
-
 end
 
-L_inf_dist(a,b)=maximum(abs.(a-b))
+
 hist = Float64[]
 for Q in re_OneDim.Q_List
     dist = maximum(map(L_inf_dist, Q, re_OneDim_fixpoint.Q))
@@ -108,8 +108,9 @@ function TwoDimTest4_PI1(Nh::Int64)
     F1(m) = m^0.5
     F2(m) = 0
     update_Q(Du,m) = Du / F1(m)
+
     problem = MFGTwoDim(xmin1,xmax1,xmin2,xmax2,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem;node1=Nh,node2=Nh,N=50) 
+    re = solve_mfg(problem;method=:PI1,node1=Nh,node2=Nh,N=50) 
     return re
 end
 
@@ -123,7 +124,7 @@ function TwoDimTest4_PI2(Nh::Int64)
     F1(m) = m^0.5
     F2(m) = 0
     update_Q(Du,m) = Du / F1(m)
-    # Nh=100
+
     problem = MFGTwoDim(xmin1,xmax1,xmin2,xmax2,T,ε,m0,uT,V,F1,F2,update_Q) 
     re_algo2 = solve_mfg(problem;method=:PI2,node1=Nh,node2=Nh,N=50,verbose=true)
     return re_algo2
@@ -139,14 +140,15 @@ function TwoDimTest4_fixpoint(Nh::Int64)
     F1(m) = m^0.5
     F2(m) = 0
     update_Q(Du,m) = Du / F1(m)
-    # Nh = 100
+
     problem = MFGTwoDim(xmin1,xmax1,xmin2,xmax2,T,ε,m0,uT,V,F1,F2,update_Q) 
     re_algo2 = solve_mfg_fixpoint(problem;method=:FixPoint2,node1=Nh,node2=Nh,N=50,maxit=100,verbose=true)
     return re_algo2
 end
 re_PI1 = TwoDimTest4_PI1(50)
 re_PI2_Nh50 = TwoDimTest4_PI2(50)
-re_fixpoint2_Nh50 = TwoDimTest4_fixpoint(50) # q* get when |m^{k+1}-m^{k}|<1e-10
+re_fixpoint2_Nh50 = TwoDimTest4_fixpoint(50) 
+# q* get when |m^{k+1}-m^{k}|<1e-10
 
 
 # Mass policy iteration
@@ -175,22 +177,21 @@ savefig("figures/residual_FP_Nh50.pdf")
 plot(re_PI1.history.residual_HJB, yaxis=:log, label="PI1-HJB")
 plot!(re_PI2_Nh50.history.residual_HJB, linestyles=:dash, yaxis=:log, label="PI2-HJB")
 # savefig("residual_HJB_PI1_2.pdf")
-
 plot!(re_PI1.history.residual_FP, yaxis=:log, label="PI1-FP")
 plot!(re_PI2_Nh50.history.residual_FP, linestyles=:dash, yaxis=:log, label="PI2-FP")
 savefig("figures/residual_PI1_2.pdf")
 
 begin
 # converge case2 PI1&PI2 |q^{k+1}-q^{k}|
-    plot(re_PI1.history.hist_q, yaxis=:log, label="PI1")
-    plot!(re_PI2_Nh50.history.hist_q, yaxis=:log, label="PI2")
-    savefig("converge_case2_q.pdf")
-    plot(re_PI1.history.hist_m, yaxis=:log, label="PI1")
-    plot!(re_PI2_Nh50.history.hist_m, yaxis=:log, label="PI2")
-    savefig("converge_case2_m.pdf")
-    plot(re_PI1.history.hist_u, yaxis=:log, label="PI1")
-    plot!(re_PI2_Nh50.history.hist_u, yaxis=:log, label="PI2")
-    savefig("converge_case2_u.pdf")
+    # plot(re_PI1.history.hist_q, yaxis=:log, label="PI1")
+    # plot!(re_PI2_Nh50.history.hist_q, yaxis=:log, label="PI2")
+    # savefig("converge_case2_q.pdf")
+    # plot(re_PI1.history.hist_m, yaxis=:log, label="PI1")
+    # plot!(re_PI2_Nh50.history.hist_m, yaxis=:log, label="PI2")
+    # savefig("converge_case2_m.pdf")
+    # plot(re_PI1.history.hist_u, yaxis=:log, label="PI1")
+    # plot!(re_PI2_Nh50.history.hist_u, yaxis=:log, label="PI2")
+    # savefig("converge_case2_u.pdf")
 end
 
 # |q^{k}-q^*|
