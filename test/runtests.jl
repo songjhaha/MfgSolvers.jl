e889417d155f72e8c5f46ab897010f52f84d0ca5
songjhaha <songjh96@foxmail.com>
2021-09-20 14:10:30 +0800

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,105 +1,75 @@
 using Test
 using MfgSolvers
 
-function OneDimTest1()
-    xmin = 0.0
-    xmax = 1.0
-    T = 1
-    ε = 0.05
-    m0(x) = ((x>=0.375) && (x<=0.625)) ? 1 : 0
-    uT(x) = 100min( (x-0.3)^2, (x-0.7)^2 )
+function OneDimTest()    
+    xmin = 0.0    
+    xmax = 1.0    
+    T = 1    
+    ε = 0.05    
+    m0(x) = ((x>=0.375) && (x<=0.625)) ? 4 : 0
+    uT(x) = 10min( (x-0.3)^2, (x-0.7)^2 )
     V(x) = 0.1
-    F1(m) = 20(1+4m)^1.5
+    F1(m) = (1+4m)^1.5
     F2(m) = m
     update_Q(Du,m) = Du / F1(m)
     problem = MFGOneDim(xmin,xmax,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem;node=50, N=100, verbose=true)
-    return re.converge
+    re = solve_mfg(problem;method=:PI1,node=200,N=200,verbose=true)
+    re2 = solve_mfg(problem;method=:PI2,node=200,N=200,verbose=true)
+    re_fixp = solve_mfg_fixpoint(problem;method=:FixPoint2,node=200,N=200,verbose=true)
+    return (re.converge,  re2.converge, re_fixp.converge)
 end
 
-function OneDimTest2()
-    xmin = 0.0
-    xmax = 1.0
-    T = 1
-    ε = 0.01
-    m0(x) = ((x>=0.375) && (x<=0.625)) ? 1 : 0
-    uT(x) = 150min( (x-0.3)^2, (x-0.7)^2 )
-    V(x) = 0.1
-    F1(m) = 20(1+4m)^0.75
-    F2(m) = m
-    function update_Q(Du, m)
-        out = Du / F1(m)
-        if out > 0.1
-            out = 0.1
-        elseif out < -0.1
-            out = -0.1
-        end
-        return out
-    end
-    problem = MFGOneDim(xmin,xmax,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem;node=50, N=100, verbose=true)
-    return re.converge
-end
-
-function TwoDimTestSimple()
-    # simple 2d case without meanfield
+function TwoDimTest()
     xmin1, xmax1, xmin2, xmax2 = 0, 1, 0, 1
-    T = 1
-    ε = 1
-    m0(x1,x2) = exp(-40((x1-0.5)^2+(x2-0.5)^2))
-    uT(x1,x2) = (cospi(2*x1) + cospi(2*x2))
-    V(x1,x2) = 0
-    F1(m) = 1
+    T = 0.5
+    ε = 0.3
+    m0(x1,x2) = exp(-10((x1-0.25)^2+(x2-0.25)^2))
+    uT(x1,x2) = 1.2*cospi(2*x1) + cospi(2*x2)
+    V(x1,x2) = 0.1
+    F1(m) = m^0.5
     F2(m) = 0
     update_Q(Du,m) = Du / F1(m)
     problem = MFGTwoDim(xmin1,xmax1,xmin2,xmax2,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem;node1=50, node2=50, N=100, verbose=true)
-    return re.converge
+    re = solve_mfg(problem;method=:PI1,node1=50,node2=50,N=50) 
+    re2 = solve_mfg(problem;method=:PI2,node1=50,node2=50,N=50)
+    re_fixp = solve_mfg_fixpoint(problem;method=:FixPoint2,node1=50,node2=50,N=50)  
+    return (re.converge,  re2.converge, re_fixp.converge)
 end
 
-function OneDimTestPI2()
-    xmin = 0.0
-    xmax = 1.0
-    T = 1
-    ε = 0.05
-    m0(x) = ((x>=0.375) && (x<=0.625)) ? 1 : 0
-    uT(x) = 100min( (x-0.3)^2, (x-0.7)^2 )
-    V(x) = 0.1
-    F1(m) = 20(1+4m)^1.5
-    F2(m) = m
-    update_Q(Du,m) = Du / F1(m)
-    problem = MFGOneDim(xmin,xmax,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem; method=:PI2, node=50, N=100, verbose=true)
-    return re.converge
-end
-
-function TwoDimTestPI2()
-    # simple 2d case without meanfield
+function TwoDim_non_quad_Test()
     xmin1, xmax1, xmin2, xmax2 = 0, 1, 0, 1
-    T = 1
-    ε = 1
-    m0(x1,x2) = exp(-40((x1-0.5)^2+(x2-0.5)^2))
-    uT(x1,x2) = (cospi(2*x1) + cospi(2*x2))
-    V(x1,x2) = 0
-    F1(m) = 1
+    T = 0.5
+    ε = 0.3
+    m0(x1,x2) = exp(-10((x1-0.25)^2+(x2-0.25)^2))
+    uT(x1,x2) = 1.2*cospi(2*x1) + cospi(2*x2)
+    V(x1,x2) = 0.1
+    F1(m) = m^0.25
     F2(m) = 0
-    update_Q(Du,m) = Du / F1(m)
+    update_Q(DU, Du_norm, m) = DU*Du_norm / (m^0.5)
     problem = MFGTwoDim(xmin1,xmax1,xmin2,xmax2,T,ε,m0,uT,V,F1,F2,update_Q)
-    re = solve_mfg(problem; method=:PI2, node1=50, node2=50, N=100, verbose=true)
-    return re.converge
+    re = solve_mfg_non_quad(problem;method=:PI1, node1=50,node2=50,N=50) 
+    re2 = solve_mfg_non_quad(problem;method=:PI2, node1=50,node2=50,N=50) 
+    return (re.converge,  re2.converge)
 end
 
 
 @testset "OneDim" begin
-    @test OneDimTest1()
-    @test OneDimTest2()
+    converge1, converge2, converge_fp = OneDimTest() 
+    @test converge1
+    @test converge2
+    @test converge_fp
 end
 
 @testset "TwoDim" begin
-    @test TwoDimTestSimple()
+    converge1, converge2, converge_fp = TwoDimTest() 
+    @test converge1
+    @test converge2
+    @test converge_fp
 end
 
-@testset "Alogrithm2" begin
-    @test OneDimTestPI2()
-    @test TwoDimTestPI2()
+@testset "NonQuad" begin
+    converge1, converge2 = TwoDim_non_quad_Test() 
+    @test converge1
+    @test converge2
 end
+
